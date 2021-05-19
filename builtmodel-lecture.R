5c42d5ce6435e7df17b913ce01aa8b92468e896d
Alina Gombert <82286235+Kompostieren@users.noreply.github.com>
2021-05-19 14:21:37 +0200

    Add files via upload

---
+++ builtmodel-lecture.R
@@ -0,0 +1,23 @@
+##build model
+library(decisionSupport)
+
+example_decision_inputs <- read.csv("example_decision_inputs.csv")
+
+example_decision_inputs$variable
+str(example_decision_inputs)
+
+
+example_decision_model <- function(x, varnames)
+{
+  profit <- benefits-costs
+  
+  final_profits <- profit + 500
+  
+  return(final_profits)
+  
+}
+
+mcSimulation(estimate = as.estimate(example_decision_inputs),
+             model_function = example_decision_model,
+             numberOfModelRuns = 100,
+             functionSyntax = "plainNames")
