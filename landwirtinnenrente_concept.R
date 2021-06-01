d964e083ac6a61fd201a1139e8b1f7aa6c7f5c19
kompostieren <gombert.alina@gmail.com>
2021-06-01 11:23:30 +0200

    new model

---
+++ landwirtinnenrente_concept.R
@@ -0,0 +1,21 @@
+
+##built model seminar
+
+library(DiagrammeR)
+
+##hello there
+##This is a seminar assignment, but very interesting
+##change
+##change2
+##change3
+
+
+##1
+mermaid("graph LR
+        Y(Yield)-->I(Income); linkStyle 0 stroke:green, stroke-width:1.5px
+        M(Market price)-->I; linkStyle 1 stroke: green, stroke-width:1.5px
+        I-->F(Final result); linkStyle 2 stroke: green, stroke-width:1.5px
+        CL(Labor cost)-->F; linkStyle 3 stroke: red, stroke-width:1.5px
+        CM(Management cost)-->F; linkStyle 4 stroke: red, stroke-width:2.5px")
+
+##2
